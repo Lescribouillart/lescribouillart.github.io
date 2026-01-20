aa07532ecadbb5937111452612ba5ce3ce6ac438
Drelall <137816034+Drelall@users.noreply.github.com>
2026-01-20 15:56:16 +0100

    maj

---
+++ java-script/contenu_footer.js
@@ -0,0 +1,12 @@
+// Contenu des différentes pages du footer
+const contenuPagesFooter = {
+    // Suppression des sections 'a-propos' et 'contact'
+};
+
+// Supprimer les fonctions qui ne servent plus
+// function afficherPageFooter() - SUPPRIMÉE
+// function fermerPageFooter() - SUPPRIMÉE
+// function gererSoumissionContactWeb3Forms() - SUPPRIMÉE
+// function showMessageInForm() - SUPPRIMÉE
+// function gererSoumissionContact() - SUPPRIMÉE
+// function initialiserLinksFooter() - SUPPRIMÉE
